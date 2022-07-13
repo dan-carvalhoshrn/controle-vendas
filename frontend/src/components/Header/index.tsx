0427e317c04ad86aaf246cd5a13b14b6e3398737
Daniel Carvalho <daniel.carvalhoshrn@gmail.com>
2022-07-12 22:40:07 -0300

    Other Components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import logo from '../../assets/img/logo.svg'
+
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="logo-container">
+                <img src={logo} alt="DSMeta"/>
+                    <h1>DSMeta</h1>
+                    <p>
+                        Desenvolvido por
+                        <a href="https://www.linkedin.com/in/daniel-carvalhosh/">daniel-carvalhoshrn</a>
+                    </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
