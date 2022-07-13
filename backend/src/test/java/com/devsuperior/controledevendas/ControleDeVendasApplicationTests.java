cf14798834eee0913cd8b7af632369e03fe48bcc
Daniel Carvalho <daniel.carvalhoshrn@gmail.com>
2022-07-12 21:34:31 -0300

    Project Created

---
+++ backend/src/test/java/com/devsuperior/controledevendas/ControleDeVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.controledevendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDeVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
